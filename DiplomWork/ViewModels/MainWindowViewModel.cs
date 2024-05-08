3502f456acb888c99207b0eb7d11ad8ce2dbcf7b
Timur <timkas040305@gmail.com>
2024-05-08 10:59:27 +0300

    подключил MainWindowViewModel к MainWindow

---
+++ DiplomWork/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DiplomWork.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomWork.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "Диплом";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        } 
+        #endregion
+    }
+}
