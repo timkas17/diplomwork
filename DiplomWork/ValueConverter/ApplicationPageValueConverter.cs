817d6c2f9582c0bdf54f1c76343c8aac152631f4
Timur <timkas040305@gmail.com>
2024-05-08 13:59:29 +0300

    Создал класс ApplicationPageConverter для переключения между страницами

--- DiplomWork/ValueConverter/ApplicationPageValueConverter.cs
+++ DiplomWork/ValueConverter/ApplicationPageValueConverter.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+using System.Diagnostics;
+using DiplomWork.Pages;
+using System.Net.Http.Headers;
 
 namespace DiplomWork.ValueConverter
 {
-    class ApplicationPageValueConverter
+    /// <summary>
+    /// Класс конвертирует <see cref="ApplicationPage"/> в актуальную страницу/представление
+    /// </summary>
+    public class ApplicationPageValueConverter : BasicValuseConverter<ApplicationPageValueConverter>
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Нахождение соответствующей страницы
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Login:
+                    return new LoginPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parametr, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
