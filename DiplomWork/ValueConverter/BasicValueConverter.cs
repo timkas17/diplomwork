0cd43901fca88d7be7248aac2077965c35c7efe6
Timur <timkas040305@gmail.com>
2024-05-08 12:44:46 +0300

    чистка кода BasicValueConverter

--- DiplomWork/ValueConverter/BasicValueConverter.cs
+++ DiplomWork/ValueConverter/BasicValueConverter.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Windows.Data;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace DiplomWork.ValueConverter
